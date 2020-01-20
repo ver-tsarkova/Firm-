d4ce51f2b777c9b2fa852a16a9cf7ef4030b59dd
ver-tsarkova <32442706+ver-tsarkova@users.noreply.github.com>
2020-01-19 18:34:03 -0800

    Add files via upload

---
+++ Payable.java
@@ -0,0 +1,13 @@
+/**
+* Payable.java
+* @author Veronica Tsarkova
+* @since 11/30/18
+*/
+
+public interface Payable
+{
+  //------------------------------------------------------------------------
+  // Derived classes must define the pay method for each type of employee
+  //------------------------------------------------------------------------
+  public double pay();
+}
